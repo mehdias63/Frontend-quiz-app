df5a6e5b58d6f75fda7f534745eb5828e49cfe32
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-06-09 21:22:24 +0330

    add button

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,8 +1,11 @@
+// import { Button } from '../../components/components/ui/button'
+
 function Header({ title, icon }) {
 	return (
 		<div className="flex items-center space-x-2 mb-6">
 			<img src={icon} alt={title} className="w-10 h-10" />
 			<h2 className="text-2xl font-bold">{title}</h2>
+			{/* <Button variant="outline">Button</Button> */}
 		</div>
 	)
 }
